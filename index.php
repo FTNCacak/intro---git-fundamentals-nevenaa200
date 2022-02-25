263095a4973da31a929f78ace98d7a5b8381711e
nevenaa200 <72276100+nevenaa200@users.noreply.github.com>
2022-02-25 20:40:17 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+</head>
+<body bgcolor="yellow" text="green">
+<h1> Naslov H1 </h1>
+<h2> Naslov H2 </h2>
+<h3> Naslov H3 </h3>
+
+<p align="left">levo </p>
+<p align="center">centrirano </p>
+<p align="right">desno</p>
+
+
+<font face="Comic Sans MS"> Ovde dodje vas tekst. </font>
+<font color="#ff0000"> Ovde ide vas tekst. </font>
+<font size="5"> Ovde ide vas tekst. </font>
+
+<ol >
+	<li>prolece</li>
+	<li>leto</li>
+	<li>jesen</li>
+	<li>zima</li>
+</ol>
+<dl>
+	<dt>Prolece:
+	<dd> od 21. 03 do 21. 06.
+	<dt>Leto:
+	<dd> od 21. 06 do 21. 09.
+	<dt>Jesen:
+	<dd> od 21. 09 do 21. 12.
+	<dt>Zima:
+	<dd> od 21. 12 do 21. 03.
+</dl>
+<img src="icon.png" align="left" height="50" width="50" hspace="20" vspace="20" border="3">
+
+<a href="linksOnPage.html">Link ka stranici za vezbu linkova</a>
+<a href="http://www.ftn.kg.ac.rs/"> FTN link</a>
+</body>
+</html>
